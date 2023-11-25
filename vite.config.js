98ef01150202733bbd31b0d2c4ba836b2aa8f316
Maksim Makarenko <makmakale87@gmail.com>
2023-11-25 16:21:16 +0300

    added deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,28 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({
-    include: /\.(jsx|tsx)$/,
-    babel: {
-      plugins: ['styled-components'],
-      babelrc: false,
-      configFile: false,
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    plugins: [react({
+      include: /\.(jsx|tsx)$/,
+      babel: {
+        plugins: ['styled-components'],
+        babelrc: false,
+        configFile: false,
+      },
+    })],
+    resolve: {
+      alias: {
+        '@': `${path.resolve(__dirname, 'src')}`,
+      },
+      extensions: ['.js', '.jsx'],
     },
-  })],
-  resolve: {
-    alias: {
-      '@': `${path.resolve(__dirname, 'src')}`,
-    },
-    extensions: ['.js', '.jsx'],
-  },
+  };
+
+  if (command !== 'serve') {
+    config.base = '/profile/';
+  }
+
+  return config;
 });
