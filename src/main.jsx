8e06ca3b56863ebe03cf54965306cfc150c061a1
Maksim Makarenko <makmakale87@gmail.com>
2023-11-25 13:15:45 +0300

    #12 avoid console messages on production

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,13 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 
+function noop() {
+}
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
