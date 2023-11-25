fe9b2e0961a8de7a9ad7305789d117999e4f8aef
Maksim Makarenko <makmakale87@gmail.com>
2023-11-25 15:22:52 +0300

    revert vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV !== 'development' ? '/profile/' : './',
+  base: '/profile/',
   plugins: [react({
     include: /\.(jsx|tsx)$/,
     babel: {
